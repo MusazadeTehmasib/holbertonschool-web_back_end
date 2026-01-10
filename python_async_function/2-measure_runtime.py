1363581fc29b6545e7e64d8cddf443aec2241f40
MusazadeTehmasib <tehmasibmusazade@email.com>
2026-01-10 21:09:19 +0400

    d

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,28 +1,17 @@
 #!/usr/bin/env python3
-"""
-Bu modul wait_n coroutine-in icra müddətini ölçür.
-"""
 
 import time
 import asyncio
+from typing import Union
 
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
-    """
-    wait_n coroutine-in ümumi icra vaxtını ölçür və
-    orta icra müddətini (total_time / n) qaytarır.
 
-    Args:
-        n (int): Coroutine sayı
-        max_delay (int): Maksimum gecikmə müddəti
-
-    Returns:
-        float: Orta icra müddəti
-    """
-    start_time: float = time.time()
+    start = time.perf_counter()
     asyncio.run(wait_n(n, max_delay))
-    end_time: float = time.time()
+    end = time.perf_counter()
 
-    return (end_time - start_time) / n
+    total_time = end - start
+    return total_time / n
